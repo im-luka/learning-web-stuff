a9f037ff27061abd02aa754e25fffd5195d9877f
im-luka <lux.9833@gmail.com>
2023-02-02 18:40:19 +0100

    next 01: data fetching

--- next/01-next13-intro/app/page.tsx
+++ next/01-next13-intro/app/page.tsx
@@ -1,7 +1,30 @@
-export default function Home() {
+interface Time {
+  dateTime: number;
+}
+
+async function getCurrentTime() {
+  const res = await fetch(
+    "https://www.timeapi.io/api/Time/current/zone?timeZone=Europe/Zagreb",
+    {
+      cache: "no-store",
+      headers: {
+        "x-foo": "bar",
+      },
+    }
+  );
+
+  const time: Promise<Time> = await res.json();
+  return time;
+}
+
+export default async function SSRPage() {
+  const time = await getCurrentTime();
+  console.log(time);
+
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <>
+      <h1>Time in Chicago (SSR)</h1>
+      <p>{new Date(time.dateTime).toString()}</p>
+    </>
   );
 }
