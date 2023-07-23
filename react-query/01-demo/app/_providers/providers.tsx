ac0d9df07e2ed4195e049a52db076827105dbdea
im-luka <lux.9833@gmail.com>
2023-07-23 15:13:00 +0200

    rq 01: setting up devtools

--- react-query/01-demo/app/_providers/providers.tsx
+++ react-query/01-demo/app/_providers/providers.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { FC, ReactNode } from "react";
 
 type Props = {
@@ -11,6 +12,9 @@ export const Providers: FC<Props> = ({ children }) => {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      {children}
+    </QueryClientProvider>
   );
 };
