7bbb54718062b60d59bb1b63d6902f433a1d95b8
im-luka <lux.9833@gmail.com>
2023-08-04 15:08:41 +0200

    rq 01: optimistic updates

--- react-query/01-demo/hooks/use-superheroes.ts
+++ react-query/01-demo/hooks/use-superheroes.ts
@@ -18,11 +18,23 @@ export const useAddSuperhero = () => {
 
   return useMutation(addHero, {
     // onSuccess: () => qc.invalidateQueries(["superheroes"]),
-    onSuccess: (data: any) => {
-      qc.setQueryData(["superheroes"], (oldData: any) => [
-        ...oldData,
-        data.data,
-      ]);
+    // onSuccess: (data: any) => {
+    //   qc.setQueryData(["superheroes"], (oldData: any) => [
+    //     ...oldData,
+    //     data.data,
+    //   ]);
+    // },
+    onMutate: async (newHero) => {
+      await qc.cancelQueries(["superheroes"]);
+      const prevHeroData = qc.getQueryData(["superheroes"]);
+      qc.setQueryData(["superheroes"], (oldData: any) => [...oldData, newHero]);
+      return { prevHeroData };
+    },
+    onError: (error: any, variables: any, context: any) => {
+      qc.setQueryData(["superheroes"], context.prevHeroData);
+    },
+    onSettled: () => {
+      qc.invalidateQueries(["superheroes"]);
     },
   });
 };
