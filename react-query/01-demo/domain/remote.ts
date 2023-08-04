dd29d18c2756c1642eba64a018af744f57b2f34f
im-luka <lux.9833@gmail.com>
2023-08-04 15:14:36 +0200

    rq 01: axios interceptor

--- react-query/01-demo/domain/remote.ts
+++ react-query/01-demo/domain/remote.ts
@@ -3,3 +3,10 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "http://localhost:3000/api",
 });
+
+export const request = ({ ...options }) => {
+  api.defaults.headers.common.Authorization = "Bearer (...token)";
+  const onSuccess = (res: any) => res;
+  const onError = (err: any) => err; // do something with error...
+  return api(options).then(onSuccess).catch(onError);
+};
