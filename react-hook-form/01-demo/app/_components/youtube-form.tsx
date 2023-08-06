72e7cebdb4d86585bf51aea6440dab7f8b1eda67
im-luka <lux.9833@gmail.com>
2023-08-06 17:15:48 +0200

    rhf 01: async validation

--- react-hook-form/01-demo/app/_components/youtube-form.tsx
+++ react-hook-form/01-demo/app/_components/youtube-form.tsx
@@ -137,6 +137,13 @@ export const YoutubeForm: FC = () => {
                 notBlackListed: (fieldValue) =>
                   !fieldValue.endsWith("baddomain.com") ||
                   "This domain is not supported.",
+                emailAvailable: async (fieldValue) => {
+                  const response = await fetch(
+                    `https://jsonplaceholder.typicode.com/users?email=${fieldValue}`
+                  );
+                  const data = await response.json();
+                  return data.length === 0 || "Email already exists.";
+                },
               },
             })}
           />
