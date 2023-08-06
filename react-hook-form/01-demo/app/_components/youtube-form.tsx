c1ada1e11074e6cb5aab5483a24667f61105c6a2
im-luka <lux.9833@gmail.com>
2023-08-06 11:26:31 +0200

    rhf 01: managing form state

--- react-hook-form/01-demo/app/_components/youtube-form.tsx
+++ react-hook-form/01-demo/app/_components/youtube-form.tsx
@@ -4,22 +4,22 @@ import { FC } from "react";
 import { useForm } from "react-hook-form";
 
 export const YoutubeForm: FC = () => {
-  const form = useForm();
+  const { register } = useForm();
 
   return (
     <div>
       <form className="flex flex-col space-y-3">
         <div className="flex flex-col">
           <label htmlFor="username">Username</label>
-          <input type="text" id="username" name="username" />
+          <input type="text" id="username" {...register("username")} />
         </div>
         <div className="flex flex-col">
           <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" />
+          <input type="email" id="email" {...register("email")} />
         </div>
         <div className="flex flex-col">
           <label htmlFor="channel">Channel</label>
-          <input type="text" id="channel" name="channel" />
+          <input type="text" id="channel" {...register("channel")} />
         </div>
         <div className="pt-5">
           <button className="w-full">Submit</button>
