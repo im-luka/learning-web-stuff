d74f58d736233929a696cc40d3f090f573bba97d
im-luka <lux.9833@gmail.com>
2023-08-06 11:02:05 +0200

    rhf 01: initial commit

---
+++ react-hook-form/01-demo/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <h1 className="mb-7 text-4xl font-bold">
+      Welcome to <span className="italic text-red-400">React Hook Form</span>
+    </h1>
+  );
+}
