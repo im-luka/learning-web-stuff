1bc2fde23a5620b0c556e04ae95f19adb0cf41b6
im-luka <lux.9833@gmail.com>
2024-03-24 10:15:06 +0100

    next 07: project init, shadcn setup and registration form & client validation

---
+++ next/07-pronextjs-forms/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { RegistrationForm } from "./registration-form";
+
+export default function Home() {
+  return (
+    <div className="mx-auto max-w-xl">
+      <RegistrationForm />
+    </div>
+  );
+}
---
+++ next/07-pronextjs-forms/src/app/registration-form.tsx
@@ -0,0 +1,85 @@
+"use client";
+
+import { FC } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { schema } from "./registration-schema";
+
+export const RegistrationForm: FC = () => {
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues: {
+      first: "",
+      last: "",
+      email: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof schema>) => {
+    console.log(data);
+  };
+
+  return (
+    <Form {...form}>
+      <form className="space-y-8" onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="flex gap-2">
+          <FormField
+            control={form.control}
+            name="first"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel>First Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="" {...field} />
+                </FormControl>
+                <FormDescription>Your first name.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="last"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel>Last Name</FormLabel>
+                <FormControl>
+                  <Input placeholder="" {...field} />
+                </FormControl>
+                <FormDescription>Your last name.</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="" {...field} />
+              </FormControl>
+              <FormDescription>Your email address.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
